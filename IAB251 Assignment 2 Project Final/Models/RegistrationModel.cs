fcfb262f7358ab9a21df2f564b26f5dd3f17e22c
Shanthu Jayamaha <shanthu.jayamaha001@gmail.com>
2024-10-10 16:49:57 +1000

    Started working on T2 User Story (added basic user input functionality)

---
+++ IAB251 Assignment 2 Project Final/Models/RegistrationModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace IAB251_Assignment_2_Project_Final.Models
+{
+	public class RegistrationModel
+	{
+
+        public string inputFirst { get; set; }
+
+        public string inputLast { get; set; }
+
+        public string inputEmail { get; set; }
+
+        public string inputPhone { get; set; }
+
+        public string inputCompany { get; set; }
+
+        public string inputAddress { get; set; }
+
+        public string inputPassword { get; set; }
+
+
+        public RegistrationModel()
+		{
+
+		}
+	}
+}
+
