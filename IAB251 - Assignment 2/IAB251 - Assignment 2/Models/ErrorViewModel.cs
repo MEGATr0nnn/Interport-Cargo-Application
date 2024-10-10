dce9add24e3136ac3e6ed44ff2b4d585ff94d3d6
Shanthu Jayamaha <166670548+ShanthuJayamaha@users.noreply.github.com>
2024-10-10 16:25:10 +1000

    Delete IAB251 - Assignment 2 directory

--- IAB251 - Assignment 2/IAB251 - Assignment 2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IAB251___Assignment_2.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
