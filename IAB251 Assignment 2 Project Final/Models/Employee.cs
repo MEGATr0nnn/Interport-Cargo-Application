d6b9f0f9565cd7e98fdf2f7eefb4bd467fd8a766
ShanthuJayamaha <shanthu.jayamaha001@gmail.com>
2024-11-04 20:56:41 +1000

    Added tests

--- IAB251 Assignment 2 Project Final/Models/Employee.cs
+++ IAB251 Assignment 2 Project Final/Models/Employee.cs
@@ -5,7 +5,9 @@
     /// </summary>
     public class Employee : IUser
     {
-        //fields
+        /// <summary>
+        /// All necessary Employee fields
+        /// </summary>
         private int _id;
         private string _firstName;
         private string _lastName;
@@ -13,6 +15,13 @@
         private string _phoneNumber;
         private string _password;
         private string _type;
+
+        /// <summary>
+        /// List of all valid Employee Types
+        /// </summary>
+        public List<string> EmployeeTypes = new List<string>{"Admin", "Quotation Officer", 
+                                                            "Booking Officer", "Warehouse Officer", 
+                                                            "Manager", "CIO"};
 
         /// <summary>
         /// Constructor responsible for creating Employees.
@@ -101,6 +110,77 @@
         public void setType(string type)
         {
             this._type=type;
+        }
+
+        /// <summary>
+        /// Test to determine valid email
+        /// </summary>
+        /// <param name="email">Employee Email Input</param>
+        /// <returns>Boolean for valid email</returns>
+        public bool testValidEmail(string email)
+        {
+            if (email.Contains('@'))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Test to determine valid phone number
+        /// </summary>
+        /// <param name="number">Employee Email Input</param>
+        /// <returns>Boolean for Valid Phone Number</returns>
+        public bool testValidNumber(string number)
+        {
+            if (number.Length == 10)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Test to determine that all characters are digits in provided phone number
+        /// </summary>
+        /// <param name="phoneNumber">Employee Phone Number Input</param>
+        /// <returns>Boolean for Valid Format</returns>
+        public bool testPhoneNumbersOnly(string phoneNumber)
+        {
+            for (int i = 0; i < phoneNumber.Length; i++)
+            {
+                if (!Char.IsDigit(phoneNumber[i])){
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Test to determine valid employee type
+        /// </summary>
+        /// <param name="employeeType">The type of employee from user input</param>
+        /// <returns>Boolean for valid employee type</returns>
+        public bool testVaildEmployeeType(string employeeType)
+        {
+            foreach (string type in EmployeeTypes)
+            {
+                if (employeeType.Equals(type))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool testDuplicateEmail(string email1, string email2)
+        {
+            if (email1.Equals(email2))
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
